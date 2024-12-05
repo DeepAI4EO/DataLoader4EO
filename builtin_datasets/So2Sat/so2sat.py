e464c9c7cd266a47584c68a08c7f3dc879d63367
xiong-zhitong <xiongzhitong@gmail.com>
2024-12-05 22:35:17 +0000

    update

--- builtin_datasets/So2Sat/so2sat.py
+++ builtin_datasets/So2Sat/so2sat.py
@@ -1,15 +1,27 @@
 import dataset4eo as eodata
 import litdata as ld
 import time
+from huggingface_hub import snapshot_download
 
-train_dataset = eodata.StreamingDataset(input_dir="/data/chen/dataset4eo/outputs/So2Sat/train", num_channels=18, channels_to_select=[0, 3, 5, 7, 9], shuffle=True, drop_last=True)
+
+repo_id = eodata.builtin_datasets['so2sat']
+
+local_path = snapshot_download(
+    repo_id=repo_id,
+    repo_type = "dataset",
+    cache_dir="./data_so2sat_cls",  # Custom directory
+    revision="main"                 # Specific branch, tag, or commit
+)
+
+split = "train"
+
+train_dataset = eodata.StreamingDataset(input_dir=f"{local_path}/{split}", num_channels=18, channels_to_select=[0, 3, 5, 7, 9], shuffle=True, drop_last=True)
 train_dataloader = ld.StreamingDataLoader(train_dataset)
 
 iters = 0
 start = time.time()
 for sample in train_dataloader:
     img, cls = sample['image'], sample['class']
-    print(img.shape, cls)
     iters += 1
     if iters == 100:
         break
--- dataset4eo/__init__.py
+++ dataset4eo/__init__.py
@@ -1,2 +1,10 @@
 from dataset4eo.utils import optimize
 from dataset4eo.streaming import StreamingDataset
+
+
+builtin_datasets = {
+    "so2sat": 'snchen1230/So2Sat',
+    "enmap_nlcd": 'XShadow/enmap_nlcd',
+    "enmap_corine": 'XShadow/enmap_corine',
+    "enmap_cdl": 'XShadow/enmap_cdl',
+    }
